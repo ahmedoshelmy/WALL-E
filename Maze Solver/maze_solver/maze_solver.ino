4e1aef6936d0f886af37353ab18da1f5262928f5
MohamedSamir245 <mohamedsamir2452002@gmail.com>
2023-03-09 13:44:50 +0200

    fixing Shortest Algorithm

--- Maze Solver/maze_solver/maze_solver.ino
+++ Maze Solver/maze_solver/maze_solver.ino
@@ -39,6 +39,8 @@ int motor_rA = 9;
 int motor_rB = 12;///connect IN4 with 12
 
 
+bool calculateShortestAgain=false;
+
 void Forward()
 {
   digitalWrite(motor_lA, 1);
@@ -133,97 +135,116 @@ void MiniRight()
 
 void GOT_SHORTEST_PATH(char MAZE_ARRAY[], int SIZE_OF_ARRAY)
 {
-  int pIndex=0;
-  for(int i=0;i<SIZE_OF_ARRAY;i++)
-  {
-    if(MAZE_ARRAY[i]!=0)
+    int pIndex = 0;
+    for (int i = 0; i < SIZE_OF_ARRAY; i++)
     {
-      path[pIndex++]=MAZE_ARRAY[i];
+        if (MAZE_ARRAY[i] != 0)
+        {
+            path[pIndex++] = MAZE_ARRAY[i];
+        }
+        if (!calculateShortestAgain && MAZE_ARRAY[i] == 'B')
+        {
+            calculateShortestAgain = true;
+        }
     }
+    actualSize = pIndex;
 
-  }
-  actualSize=pIndex;
+
+    // This part was added due to this example [SLBSLLBLBLBLLRBLLBLLL]
+    if (calculateShortestAgain)
+    {
+        CALCULATE_SHORTEST_PATH(path, actualSize);
+    }
 }
 
 void CALCULATE_SHORTEST_PATH(char MAZE_ARRAY[], int SIZE_OF_ARRAY)
 {
-  /*ONCE THE ROBOT COMPLETES THE MAZE THE FINAL SHORTEST PATH CALCULATED
-  IS STORED IN THE ROBOT MEMORY.THIS SHORTEST PATH IS USED TO COMPLETE
-  THE SAME MAZE IN SHORTEST AMOUNT OF TIME.(L :LEFT, R:RIGHT, B:BACK,S:STRAIGHT)
-  BELOW ARE THE FEW SUBSTITUTIONS TO CONVERT FULL MAZE PATH TO ITS 
-  SHORTEST PATH:
-  LBL = S
-  LBR = B
-  LBS = R
-  RBL = B
-  SBL = R
-  SBS = B
-  LBL = S */
+    /*ONCE THE ROBOT COMPLETES THE MAZE THE FINAL SHORTEST PATH CALCULATED
+    IS STORED IN THE ROBOT MEMORY.THIS SHORTEST PATH IS USED TO COMPLETE
+    THE SAME MAZE IN SHORTEST AMOUNT OF TIME.(L :LEFT, R:RIGHT, B:BACK,S:STRAIGHT)
+    BELOW ARE THE FEW SUBSTITUTIONS TO CONVERT FULL MAZE PATH TO ITS
+    SHORTEST PATH:
+    LBL = S
+    LBR = B
+    LBS = R
+    RBL = B
+    SBL = R
+    SBS = B
+    LBL = S */
 
-  char ACTION;
-  
-  for(int i = 0; i <= SIZE_OF_ARRAY-2; i++)
+    char ACTION;
+
+    for (int i = 0; i <= SIZE_OF_ARRAY - 2; i++)
     {
-      ACTION = MAZE_ARRAY[i];
-           
-      if(ACTION == 'B')
+        ACTION = MAZE_ARRAY[i];
+
+        if (ACTION == 'B')
         {
-          if(MAZE_ARRAY[i-1]== 'L' && MAZE_ARRAY[i+1] == 'R')
+            if (MAZE_ARRAY[i - 1] == 'L' && MAZE_ARRAY[i + 1] == 'R')
             {
-              MAZE_ARRAY[i] = 'B';
-              MAZE_ARRAY[i-1] = 0;
-              MAZE_ARRAY[i+1] = 0;
-              // REARRANGE(MAZE_ARRAY,SIZE_OF_ARRAY,i-1,i,i+1); 
+                MAZE_ARRAY[i] = 'B';
+                MAZE_ARRAY[i - 1] = 0;
+                MAZE_ARRAY[i + 1] = 0;
+                // REARRANGE(MAZE_ARRAY,SIZE_OF_ARRAY,i-1,i,i+1);
+                continue;
             }
 
-           if(MAZE_ARRAY[i-1]== 'L' && MAZE_ARRAY[i+1] == 'S')
+            if (MAZE_ARRAY[i - 1] == 'L' && MAZE_ARRAY[i + 1] == 'S')
             {
-              MAZE_ARRAY[i] = 'R';
-              MAZE_ARRAY[i-1] = 0;
-              MAZE_ARRAY[i+1] = 0;
-              // REARRANGE(MAZE_ARRAY,SIZE_OF_ARRAY,i-1,i,i+1);
+                MAZE_ARRAY[i] = 'R';
+                MAZE_ARRAY[i - 1] = 0;
+                MAZE_ARRAY[i + 1] = 0;
+                // REARRANGE(MAZE_ARRAY,SIZE_OF_ARRAY,i-1,i,i+1);
+                continue;
             }
 
-            if(MAZE_ARRAY[i-1]== 'R' && MAZE_ARRAY[i+1] == 'L')
+            if (MAZE_ARRAY[i - 1] == 'R' && MAZE_ARRAY[i + 1] == 'L')
             {
-              MAZE_ARRAY[i] = 'B';
-              MAZE_ARRAY[i-1] = 0;
-              MAZE_ARRAY[i+1] = 0;
-              // REARRANGE(MAZE_ARRAY,SIZE_OF_ARRAY,i-1,i,i+1);         
+                MAZE_ARRAY[i] = 'B';
+                MAZE_ARRAY[i - 1] = 0;
+                MAZE_ARRAY[i + 1] = 0;
+                // REARRANGE(MAZE_ARRAY,SIZE_OF_ARRAY,i-1,i,i+1);
+                continue;
             }
 
-            if(MAZE_ARRAY[i-1]== 'S' && MAZE_ARRAY[i+1] == 'L')
+            if (MAZE_ARRAY[i - 1] == 'S' && MAZE_ARRAY[i + 1] == 'L')
             {
-              MAZE_ARRAY[i] = 'R';
-              MAZE_ARRAY[i-1] = 0;
-              MAZE_ARRAY[i+1] = 0;
-              // REARRANGE(MAZE_ARRAY,SIZE_OF_ARRAY,i-1,i,i+1);             
+                MAZE_ARRAY[i] = 'R';
+                MAZE_ARRAY[i - 1] = 0;
+                MAZE_ARRAY[i + 1] = 0;
+                // REARRANGE(MAZE_ARRAY,SIZE_OF_ARRAY,i-1,i,i+1);
+                continue;
             }
 
-            if(MAZE_ARRAY[i-1]== 'S' && MAZE_ARRAY[i+1] == 'S')
+            if (MAZE_ARRAY[i - 1] == 'S' && MAZE_ARRAY[i + 1] == 'S')
             {
-              MAZE_ARRAY[i] = 'B';
-              MAZE_ARRAY[i-1] = 0;
-              MAZE_ARRAY[i+1] = 0;
-              // REARRANGE(MAZE_ARRAY,SIZE_OF_ARRAY,i-1,i,i+1);             
+                MAZE_ARRAY[i] = 'B';
+                MAZE_ARRAY[i - 1] = 0;
+                MAZE_ARRAY[i + 1] = 0;
+                // REARRANGE(MAZE_ARRAY,SIZE_OF_ARRAY,i-1,i,i+1);
+                continue;
             }
 
-            if(MAZE_ARRAY[i-1]== 'L' && MAZE_ARRAY[i+1] == 'L')
+            if (MAZE_ARRAY[i - 1] == 'L' && MAZE_ARRAY[i + 1] == 'L')
             {
-              MAZE_ARRAY[i] = 'S';
-              MAZE_ARRAY[i-1] = 0;
-              MAZE_ARRAY[i+1] = 0;
-              // REARRANGE(MAZE_ARRAY,SIZE_OF_ARRAY,i-1,i,i+1);
+                MAZE_ARRAY[i] = 'S';
+                MAZE_ARRAY[i - 1] = 0;
+                MAZE_ARRAY[i + 1] = 0;
+                // REARRANGE(MAZE_ARRAY,SIZE_OF_ARRAY,i-1,i,i+1);
+                continue;
             }
-            
-          i = -1;
+
+            // i = -1;
         }
-       
-       delay(100);   
+
+        // delay(100);
     }
     shortestPathCalculated = true;
-    GOT_SHORTEST_PATH(MAZE_ARRAY,SIZE_OF_ARRAY);
+
+    calculateShortestAgain = false;
+    GOT_SHORTEST_PATH(MAZE_ARRAY, SIZE_OF_ARRAY);
 }
+
 
 void setup ()
 {
